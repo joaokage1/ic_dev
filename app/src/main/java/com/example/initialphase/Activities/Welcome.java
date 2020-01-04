915f20c6154ce19522cebd0f8518a012c2bc135e
Toddynho <j_vgomes@hotmail.com>
2020-01-04 17:23:07 -0300

    Added SignIn with Google

--- app/src/main/java/com/example/initialphase/Activities/Welcome.java
+++ app/src/main/java/com/example/initialphase/Activities/Welcome.java
@@ -1,5 +1,6 @@
 package com.example.initialphase.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,10 +10,20 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.initialphase.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Welcome extends AppCompatActivity {
 
@@ -21,6 +32,12 @@ public class Welcome extends AppCompatActivity {
     Animation uptodown,downtoup;
 
     private FirebaseAuth mAuth;
+
+    //LogIn with Google
+    static final int GOOGLE_SIGN = 123;
+    Button login_btn_gmail;
+    GoogleSignInClient mGoogleSignInClient;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +51,77 @@ public class Welcome extends AppCompatActivity {
         downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
         l1.setAnimation(uptodown);
         l2.setAnimation(downtoup);
+        progressBar = findViewById(R.id.progressBar_google);
+
+        login_btn_gmail = findViewById(R.id.button_log_gmail);
+
+        GoogleSignInOptions options = new GoogleSignInOptions
+                .Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, options);
+
+        login_btn_gmail.setOnClickListener(v -> SignInGmail());
+    }
+
+    void SignInGmail(){
+        progressBar.setVisibility(View.VISIBLE);
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, GOOGLE_SIGN);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        //updateUIWithUser(user);
+                        updateUI();
+                    } else {
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                        Toast.makeText(this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_SIGN){
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null){
+                    firebaseAuthWithGoogle(account);
+                }
+
+            }catch (ApiException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void updateUIWithUser(FirebaseUser user) {
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photo = String.valueOf(user.getPhotoUrl());
+
+
+        }
     }
 
     public void logOnClick(View view){
